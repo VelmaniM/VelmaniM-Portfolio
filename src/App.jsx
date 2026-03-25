375b50e55177426e812c566fac9d790556ee1488
velmanikandan <velmanikandan@velmanikandans-MacBook-Air.local>
2026-03-25 18:42:31 +0530

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
